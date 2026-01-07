b7799ffc9e28cea8a05dcb412353ca5250ac7487
n0thing2c <trannguyenduythinhhvn@gmail.com>
2026-01-07 08:27:02 +0700

    feat: update auth_service and router for traditional auth

--- src/routers/auth_router.py
+++ src/routers/auth_router.py
@@ -1,14 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Body
+from src.dtos.login_dto import LoginInputDto, LoginResponseDto
+from src.dtos.signup_dto import SignUpInputDto, SignUpResponseDto
+from src.repositories.user_repo import UserJsonRepo
+from src.services.auth_service import AuthService
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
-@router.post("/signup")
-def sign_up():
-    pass
 
-@router.post("/login")
-def login():
-    pass
+def get_auth_service():
+    repo = UserJsonRepo()
+    return AuthService(repo)
+
+
+@router.post("/signup", response_model=SignUpResponseDto)
+def sign_up(user: SignUpInputDto, service: AuthService = Depends(get_auth_service)):
+    return service.signUp(user)
+
+
+@router.post("/login", response_model=LoginResponseDto)
+def login(user: LoginInputDto, service: AuthService = Depends(get_auth_service)):
+    return service.login(user)
+
+
+@router.post("/refresh", response_model=LoginResponseDto)
+def refresh_token(
+    refresh_token: str = Body(..., embed=True),
+    service: AuthService = Depends(get_auth_service),
+):
+    return service.renew_access_token(refresh_token)
+
 
 @router.get("/login/google")
 def login_google():
--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -1,3 +1,76 @@
 from jose import jwt
-from datetime import datetime, timedelta
+import os
+from datetime import datetime, timedelta, timezone
+import uuid
+from fastapi import HTTPException, status
+from dotenv import load_dotenv
 
+from src.models.user import User
+from src.dtos.login_dto import LoginInputDto, LoginResponseDto
+from src.dtos.signup_dto import SignUpInputDto, SignUpResponseDto
+from src.repositories.user_repo import UserJsonRepo
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_TTL = int(os.getenv("ACCESS_TOKEN_TTL"))
+REFRESH_TOKEN_TTL = int(os.getenv("REFRESH_TOKEN_TTL"))
+
+
+def create_token(data: dict, ttl: int):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ttl)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.JWTError:
+        return None
+
+
+class AuthService:
+    def __init__(self, user_repo: UserJsonRepo):
+        self.user_repo = user_repo
+
+    def signUp(self, user: SignUpInputDto) -> SignUpResponseDto:
+        if self.user_repo.get_by_mail(user.email):
+            raise HTTPException(status_code=400, detail="Email already exists")
+
+        new_user = User(
+            id=str(uuid.uuid4()),
+            username=user.username,
+            email=user.email,
+            password=user.password,
+        )
+        created_user = self.user_repo.create(new_user)
+        return SignUpResponseDto.model_validate(created_user)
+
+    def login(self, user: LoginInputDto) -> LoginResponseDto:
+        find_user = self.user_repo.get_by_username(user.username)
+        if not find_user or find_user.password != user.password:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect email or password",
+            )
+
+        access_token = create_token(data={"sub": find_user.id}, ttl=ACCESS_TOKEN_TTL)
+        refresh_token = create_token(data={"sub": find_user.id}, ttl=REFRESH_TOKEN_TTL)
+        return LoginResponseDto(access_token=access_token, refresh_token=refresh_token)
+
+    def renew_access_token(self, refresh_token: str) -> LoginResponseDto:
+        payload = decode_token(refresh_token)
+        if not payload:
+            raise HTTPException(status_code=401, detail="Invalid refresh token")
+
+        id = payload.get("sub")
+        if not id:
+            raise HTTPException(status_code=401, detail="Invalid token content")
+        new_access_token = create_token(data={"sub": id}, ttl=ACCESS_TOKEN_TTL)
+        return LoginResponseDto(
+            access_token=new_access_token, refresh_token=refresh_token
+        )
