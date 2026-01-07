eec0f724bc95f54ad4de57f3d38b232d3ce9df66
Marvin Truong <khatruong06@gmail.com>
2026-01-08 00:20:15 +1000

    feat: add check uniqueness by username and email

--- src/repositories/user_repo.py
+++ src/repositories/user_repo.py
@@ -6,6 +6,10 @@ class UserRepo:
     async def create(self, user: User) -> User:
         await user.insert()
         return user
+    
+    async def check_unique(self, email: str, username: str) -> bool:
+        user = await User.find_one(User.email == email or User.username == username)
+        return user != None
 
     async def get_by_mail(self, email: str) -> Optional[User]:
         return await User.find_one(User.email == email)
